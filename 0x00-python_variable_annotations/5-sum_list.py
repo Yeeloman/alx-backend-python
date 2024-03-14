2e00f8c1ea561c4908ad772770fd155b3a79438e
Yeeloman <yami.onlyme@gmail.com>
2024-03-14 16:15:30 +0000

    fix task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """task 5"""
 
+from typing import List
 
-def sum_list(input_list=[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """sum list"""
     return sum(input_list)
