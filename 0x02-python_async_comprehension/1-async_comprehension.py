e05d4e546baa523cdc3262013963facb2a98f868
Yeeloman <yami.onlyme@gmail.com>
2024-03-19 15:56:32 +0000

    fix return type in task 0

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -10,4 +10,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """async comprehension"""
-    return [i async for i in async_generator()]
+    return [num async for num in async_generator()]
