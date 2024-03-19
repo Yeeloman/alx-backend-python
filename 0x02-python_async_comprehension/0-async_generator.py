712f66044ae892f1c1f0ab60e231936271918770
Yeeloman <yami.onlyme@gmail.com>
2024-03-19 15:38:09 +0000

    fix task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator() -> List[float]:
     """async generator"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
